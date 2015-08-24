e90952e8889edf582249dd814fe660412803081a
metodiew <stanko@metodiew.com>
2015-08-24 17:29:48 +0300

    Fix common issue with Total field

--- includes/class-offer-calc-shortcodes.php
+++ includes/class-offer-calc-shortcodes.php
@@ -137,7 +137,7 @@ class Offer_Calc_Shortcodes {
 						?><div class="error err-total" for="offer_total" style="display:none;">Total should be greater than zero.</div>
 						<div class="error err-submission-value" for="offer_submission_value" style="display:none;"></div>
 						<?php do_action( 'offer_calc_before_total',  $offer_post[0]->ID ) ;?>
-						<div class="total-offercalc-sum-div total_offercalc_sum"><?php _e('Total: ','offercalc');?><?php echo apply_filters('offer_calc_add_currency', $total, $offer_post[0]->ID);?></div>						
+						<div class="total-offercalc-sum-div total_offercalc_sum clr"><?php _e('Total: ','offercalc');?><?php echo apply_filters('offer_calc_add_currency', $total, $offer_post[0]->ID);?></div>						
 						<div class="clr"></div>
 						<div class="offer-calc-add-coupons-codes"><?php do_action ( 'offer_calc_add_coupons_codes', $offer_post[0]->ID, $offer_slug ); ?></div>
 						<div class="clr"></div>
